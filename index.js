e6abdc91076f6e828dd3f3634b375249f858131f
Apetsi Ampiah <apetsiampiah@gmail.com>
2019-11-05 14:54:28 +0000

    Setup cars database

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const app = express();
 
 app.use(express.json());
 
+// load database
+const db = require('./data/db-config');
+
+db('cars')
+  .where({ id: 1 })
+  .then(car => console.log(car));
+
 const port = process.env.PORT;
 
 app.listen(port, () => {
